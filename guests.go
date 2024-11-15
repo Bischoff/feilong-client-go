2d6f1afbe51d0b752adfb76f338b1aa443b6be88
Eric Bischoff <ebischoff@suse.com>
2024-11-15 16:08:07 +0100

    Reorder functions following the doc's order

--- guests.go
+++ guests.go
@@ -95,6 +95,48 @@ func (c *Client) CreateGuest(params *CreateGuestParams) (*CreateGuestResult, err
 }
 
 
+// https://cloudlib4zvm.readthedocs.io/en/latest/restapi.html#get-guest-minidisks-info
+
+type GetGuestMinidisksInfoMinidisk struct {
+	VDev		string		`json:"vdev"`
+	RDev		string		`json:"rdev"`
+	AccessType	string		`json:"access_type"`
+	DeviceType	string		`json:"device_type"`
+	DeviceSize	int		`json:"device_size"`
+	DeviceUnits	string		`json:"device_units"`
+	VolumeLabel	string		`json:"volume_label"`
+}
+
+type GetGuestMinidisksInfoOutput struct {
+	Minidisks	[]GetGuestMinidisksInfoMinidisk `json:"minidisks,omitempty"`
+}
+
+type GetGuestMinidisksInfoResult struct {
+	OverallRC	int		`json:"overallRC"`
+	ReturnCode	int		`json:"rc"`
+	Reason		int		`json:"rs"`
+	ErrorMsg	string		`json:"errmsg"`
+	ModuleId	int		`json:"modID"`
+	Output		GetGuestMinidisksInfoOutput `json:"output"`
+}
+
+func (c *Client) GetGuestMinidisksInfo(userid string) (*GetGuestMinidisksInfoResult, error) {
+	var result GetGuestMinidisksInfoResult
+
+	body, err := c.doRequest("GET", "/guests/" + userid + "/disks", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+
 // https://cloudlib4zvm.readthedocs.io/en/latest/restapi.html#guest-add-disks
 
 type AddGuestDisksParams struct {
@@ -994,48 +1036,6 @@ func (c *Client) DeleteGuestNIC(userid string, vdev string, params *DeleteGuestN
 	_, err = c.doRequest("DELETE", "/guests/" + userid + "/nic/" + vdev, body)
 
 	return err
-}
-
-
-// New method, see https://github.com/openmainframeproject/feilong/issues/796
-
-type GetGuestMinidisksInfoMinidisk struct {
-	VDev		string		`json:"vdev"`
-	RDev		string		`json:"rdev"`
-	AccessType	string		`json:"access_type"`
-	DeviceType	string		`json:"device_type"`
-	DeviceSize	int		`json:"device_size"`
-	DeviceUnits	string		`json:"device_units"`
-	VolumeLabel	string		`json:"volume_label"`
-}
-
-type GetGuestMinidisksInfoOutput struct {
-	Minidisks	[]GetGuestMinidisksInfoMinidisk `json:"minidisks,omitempty"`
-}
-
-type GetGuestMinidisksInfoResult struct {
-	OverallRC	int		`json:"overallRC"`
-	ReturnCode	int		`json:"rc"`
-	Reason		int		`json:"rs"`
-	ErrorMsg	string		`json:"errmsg"`
-	ModuleId	int		`json:"modID"`
-	Output		GetGuestMinidisksInfoOutput `json:"output"`
-}
-
-func (c *Client) GetGuestMinidisksInfo(userid string) (*GetGuestMinidisksInfoResult, error) {
-	var result GetGuestMinidisksInfoResult
-
-	body, err := c.doRequest("GET", "/guests/" + userid + "/disks", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return nil, err
-	}
-
-	return &result, nil
 }
 
 
